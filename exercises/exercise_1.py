1875a5824b3870cf2bad4f6ef88622c09c4d8dca
Lydon Syms <22396254-LydonSyms@users.noreply.replit.com>
2024-05-09 09:41:44 +0000

    Update

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,6 +1,6 @@
 # Exercise 1
-num = int(input("put in a five digit number"))
+num = int(input("put in a five digit number: "))
 a = num // 10
 b = num // 100
 c = num // 1000
-print("the total is ", a % 10 + b % 10 + c % 10 + num // 10000 + num % 10)
+print(f"{num % 10 + num // 10000 + b % 10}{a % 10 + c % 10}")
