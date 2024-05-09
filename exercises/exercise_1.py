ea4a6a490557f694cf5b33709f3757c123532d8f
L1D00N <wannabet1faker@gmail.com>
2024-05-09 10:34:49 +0100

    Update exercise_1.py

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,6 @@
 # Exercise 1
-# Your solution comes here
+num = int(input("put in a five digit number"))
+a = num // 10
+b = num // 100
+c = num // 1000
+print("the total is ", a % 10 + b % 10 + c % 10 + num // 10000 + num % 10)
