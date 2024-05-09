819c7f83d7ac2a992323585158dcdfeb86f8c80e
L1D00N <wannabet1faker@gmail.com>
2024-05-09 10:57:23 +0100

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,7 @@
 # Exercise 3
 # Your solution comes here
+num = int(input("how many seconds have passed"))
+a = num // 216000
+b = (num - (a * 216000)) // 60
+c = ((num - (a * 216000)) // 60) // 60
+print(f"{a}{':'}{b}{':'}{c}")
