13bc94e2a8c471a9186cb8ea33c7e08835456d2a
L1D00N <wannabet1faker@gmail.com>
2024-05-09 11:15:05 +0100

    Update exercise_4.py

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,10 @@
 # Exercise 4
 # Your solution comes here
+num = int(input("enter a four digit number"))
+a = num // 10
+b = num // 100
+
+if num % 10 == num // 1000 and a % 10 == b % 10:
+  print("symmetrical")
+else:
+  print("not symmetrical")
