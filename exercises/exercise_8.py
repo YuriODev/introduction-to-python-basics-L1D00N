ddd70a6da2ea16c20a91fe7e2cbcdbdef6e22337
L1D00N <wannabet1faker@gmail.com>
2024-05-13 09:32:37 +0100

    Update exercise_8.py

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,16 @@
 # Exercise 8
 # Your solution comes here
+
+a = int(input("enter your first number. "))
+b = int(input("enter your second number. "))
+c = int(input("enter your third number. "))
+
+small = a * (a <= b and a <= c) + b * (b < a or b < c) + c * (c < a and c < b)
+
+big = a * (a >= b and a >= c) + b * (b > a or b > c) + c * (c > a and c > b)
+
+mid = a + b + c - small - big
+
+print(small)
+print(mid)
+print(big)
