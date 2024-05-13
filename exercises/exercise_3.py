019ca75fdbdc03f8c0ec0a53db19812c5d9b0db4
L1D00N <wannabet1faker@gmail.com>
2024-05-13 09:19:26 +0100

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -4,4 +4,4 @@ num = int(input("how many seconds have passed"))
 a = num // 3600
 b = (num - (a * 3600)) // 60
 c = (num - (a * 3600) - (b * 60))
-print(f"{a}{':'}{b}{':'}{c}")
+print(f"{a}:{b:02d}:{c:02d}")
