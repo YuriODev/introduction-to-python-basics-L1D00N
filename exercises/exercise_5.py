12c04c37f24f3ba38d0ab609c916248f62a223d5
L1D00N <wannabet1faker@gmail.com>
2024-05-13 09:11:18 +0100

    Update exercise_5.py

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,2 +1,11 @@
 # Exercise 5
 # Your solution comes here
+num1 = int(input("enter a whole number. "))
+num2 = int(input("enter another whole number. "))
+
+if num1 > num2:
+    print(num1)
+elif num2 > num1:
+    print(num2)
+elif num1 == num2:
+    print("same")
