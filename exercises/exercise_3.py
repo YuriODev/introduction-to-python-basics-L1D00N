2f2bbdac6729ad2d0abd61a641c2687685b292f2
L1D00N <wannabet1faker@gmail.com>
2024-05-16 12:10:28 +0100

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -2,6 +2,6 @@
 # Your solution comes here
 num = int(input("how many seconds have passed"))
 a = num // 216000
-b = (num - (a * 216000)) // 60
-c = (num - (a * 216000) - (b * 60))
+b = (num - (a * 216000)) // 360
+c = (num - (a * 216000) - (b * 360))
 print(f"{a}:{b:02d}:{c:02d}")
