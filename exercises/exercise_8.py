91a8e4c7ca381a27d87bce27f5dcee08e8e677c7
L1D00N <wannabet1faker@gmail.com>
2024-05-22 13:31:59 +0100

    Update exercise_8.py

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -5,9 +5,9 @@ a = int(input("enter your first number. "))
 b = int(input("enter your second number. "))
 c = int(input("enter your third number. "))
 
-small = a * (a <= b and a <= c) + b * (b < a or b < c) + c * (c < a and c < b)
+small = (a * (a < b and a < c)) + (b * (b < a or b < c)) + (c * (c < a and c < b))
 
-big = a * (a >= b and a >= c) + b * (b > a or b > c) + c * (c > a and c > b)
+big = (a * (a > b and a > c)) + (b * (b > a or b > c)) + (c * (c > a and c > b))
 
 mid = a + b + c - small - big
 
