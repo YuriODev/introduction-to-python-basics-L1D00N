8dc131f3c513c44ed2239ce028e465de6a6d10ef
L1D00N <wannabet1faker@gmail.com>
2024-05-22 13:36:09 +0100

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,7 +1,8 @@
 # Exercise 3
 # Your solution comes here
 num = int(input("how many seconds have passed"))
-a = (num // 3600) % 24
+num = num % 24
+a = (num // 3600)
 b = (num - (a * 3600)) // 60
 c = (num - (a * 3600) - (b * 60))
 print(f"{a}:{b:02d}:{c:02d}")
